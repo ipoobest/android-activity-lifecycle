1dc3d4f4f461f61b1afe72d82136493b7be00447
Poobest Jirathitiphuwadon <>
2018-03-03 16:23:29 +0700

    Initialize project

---
+++ app/src/main/java/com/android/orc/activitylifecycler/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.android.orc.activitylifecycler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+}
